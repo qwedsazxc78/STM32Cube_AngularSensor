4771c18eecc57e801ce5004e93fc18fa51f28006
qwedsazxc78 <qwedsazxc78@gmail.com>
2018-01-24 16:59:51 +0800

    nice test in necleno F303RE

---
+++ Angular_Sensor_Demo_v1/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Angular_Sensor_Demo_v1' 
+ * Target:  'Angular_Sensor_Demo_v1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
